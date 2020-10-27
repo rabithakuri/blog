24db7e5a93b2cbb33f1e9eae27d6b838292556bd
Your Namerabithakuri <you@example.com>
2020-10-27 10:09:42 +0545

    first

---
+++ blog/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post, Comment
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+# def post_list(request):
+# 	posts = Post.published.all()
+# 	return render(request, 'blog/post/list.html', {"posts":posts})
+
+def	post_list(request):
+    object_list	=	Post.published.all()
+    paginator	=	Paginator(object_list,	3)	#	3	posts	in	each	page
+    page	=	request.GET.get('page')
+    try:
+    	posts	=	paginator.page(page)
+    except	PageNotAnInteger:
+    #	If	page	is	not	an	integer	deliver	the	first	page
+    	posts	=	paginator.page(1)
+    except	EmptyPage:
+    #	If	page	is	out	of	range	deliver	last	page	of	results
+        posts	=	paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'page':page,'posts':posts})
+
+
+
+# def post_detail(request, year , month, day, post):
+#     post = get_object_or_404(Post, slug = post, status = 'published',
+#                             publish_year = year, publish_month = month,
+#                             publish_day = day )
+#     return render(request, 'blog/post/detail.html', {'post': post})
+
+def	post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,publish__month=month, publish__day = day )
+    comments = post.comments.filter(active=True)
+    new_comment	= None
+    if	request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if	comment_form.is_valid():
+            #	Create	Comment	object	but	don't	save	to	database	yet
+            new_comment	=	comment_form.save(commit=False)
+            #	Assign	the	current	post	to	the	comment
+            new_comment.post =	post
+            #	Save	the	comment	to	the	database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return	render(request,'blog/post/detail.html',{'post':	post, 'comments':comments, 'new_comment': new_comment, 'comment_form':comment_form})
+
+
+
+from .forms import EmailPostForm,CommentForm
+
+# def post_share(request, post_id):
+#     post = get_object_or_404(Post, id=post_id,status='published')
+
+#     if	request.method	==	'POST':
+#         form	=	EmailPostForm(request.POST)
+#         if	form.is_valid:
+#             cd	=	form.cleaned_data
+#         else:
+#             form	=	EmailPostForm()
+#         return	render(request,	'blog/post/share.html',	{'post': post,'form': form})
+
+
+from django.core.mail import send_mail
+
+def	post_share(request,	post_id):
+                #	Retrieve	post	by	id
+    post = get_object_or_404(Post,	id=post_id,	status='published')
+    sent = False
+    if	request.method	==	'POST':
+        #	Form	was	submitted
+        form	=	EmailPostForm(request.POST)
+        if	form.is_valid():
+            #	Form	fields	passed	validation
+            cd	=	form.cleaned_data
+            post_url	=	request.build_absolute_uri(post.get_absolute_url())
+            subject	=	'{}	({})	recommends	you	reading	"{}"'.format(cd['name'],cd['email'],post.title)
+            message	=	'Read	"{}"	at	{}\n\n{}\'s	comments:{}'.format(post.title,	post_url,cd['name'],cd['comments'])
+            send_mail(subject,message,'admin@myblog.com',[cd['to']])
+            sent	=	True
+    else:
+        form	=	EmailPostForm()
+    return	render(request,	'blog/post/share.html',	{'post':post,'form':form,'sent':sent})
+
+
+
